1320539479aa11f6b4161eb64c9183fb8565cbb3
gansiqiu <1223720954@qq.com>
2018-05-17 15:55:04 +0800

    微调前

--- js/achieve.js
+++ js/achieve.js
@@ -7,11 +7,22 @@ $("li[id='achieveItem']").each(function(index){
 
 $("li[class='list-group-item']").each(function(index){
 	$(this).click(function(){
+		
+		$("#aaa").removeClass("animated slideInLeft");
 		$("#aaa").addClass("animated slideOutLeft");
 		$("#screenModel").removeClass("hidden");
-		$("#screenModel").removeClass("animated slideInRight");
+		
+	$("#screenModel").removeClass("animated slideOutRight");
+		$("#screenModel").addClass("animated slideInRight");
 	})
 })
 
-
+$(".clos").click(function(){
+	console.log("1111");
+	$("#screenModel").removeClass("animated slideInRight");
+	$("#screenModel").addClass("animated slideOutRight");
+	$("#aaa").removeClass("animated slideOutLeft");
+	$("#aaa").addClass("animated slideInLeft");
+	
+})
 
